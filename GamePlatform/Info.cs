f2fb4351bdc8541fb301a3c1620c40d2051f7d50
SerikRakhimov <s_astana@mail.ru>
2019-06-19 12:49:59 +0600

    first commit

---
+++ GamePlatform/Info.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace GamePlatform
+{
+    // сгенерировано с помощью https://xmltocsharp.azurewebsites.net/
+    [XmlRoot(ElementName = "link", Namespace = "http://www.w3.org/2005/Atom")]
+    public class Link
+    {
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+        [XmlAttribute(AttributeName = "href")]
+        public string Href { get; set; }
+        [XmlAttribute(AttributeName = "rel")]
+        public string Rel { get; set; }
+    }
+
+    [XmlRoot(ElementName = "source")]
+    public class Source
+    {
+        [XmlAttribute(AttributeName = "url")]
+        public string Url { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "enclosure")]
+    public class Enclosure
+    {
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+        [XmlAttribute(AttributeName = "url")]
+        public string Url { get; set; }
+    }
+
+    [XmlRoot(ElementName = "item")]
+    public class Item
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "link")]
+        public string Link2 { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "pubDate")]
+        public string PubDate { get; set; }
+        [XmlElement(ElementName = "source")]
+        public Source Source { get; set; }
+        [XmlElement(ElementName = "enclosure")]
+        public Enclosure Enclosure { get; set; }
+        [XmlElement(ElementName = "guid")]
+        public string Guid { get; set; }
+    }
+
+    [XmlRoot(ElementName = "channel")]
+    public class Channel
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "link")]
+        public List<string> Link { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "copyright")]
+        public string Copyright { get; set; }
+        [XmlElement(ElementName = "language")]
+        public string Language { get; set; }
+        [XmlElement(ElementName = "lastBuildDate")]
+        public string LastBuildDate { get; set; }
+        [XmlElement(ElementName = "item")]
+        public List<Item> Items { get; set; }
+    }
+
+    [XmlRoot(ElementName = "rss")]
+    public class Rss
+    {
+        [XmlElement(ElementName = "channel")]
+        public Channel Channel { get; set; }
+        [XmlAttribute(AttributeName = "atom")]
+        public string Atom { get; set; }
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+    }
+
+}
---
+++ GamePlatform/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace GamePlatform
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GamesLoad();
+        }
+
+        public async void GamesLoad()
+        {
+            List<Item> gamesList = new List<Item>();
+            Rss rssResult = await LoadInfo();  //await - признак асинхронной задачи
+            gamesList = rssResult.Channel.Items;
+            gamesDataGrid.ItemsSource = gamesList;
+        }
+
+        private Task<Rss> LoadInfo()
+        {
+            Rss rss = null;
+            // см. http://qaru.site/questions/44548/user-xmlns-was-not-expected-deserializing-twitter-xml
+            XmlRootAttribute xRoot = new XmlRootAttribute("rss");
+            XmlSerializer formatter = new XmlSerializer(typeof(Rss), xRoot);
+
+            WebRequest req = WebRequest.Create(@"https://gamer-info.com/rssflash/");
+            WebResponse response = req.GetResponse();
+            using (Stream s = response.GetResponseStream())  // пишем в поток.
+            {
+                using (StreamReader r = new StreamReader(s))  // читаем из потока.
+                {
+                    rss = (Rss)formatter.Deserialize(r);
+                }
+            }
+
+            response.Close(); // закрываем поток
+
+            return Task.FromResult(rss);  // правильно так писать
+        }
+
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            GamesLoad();
+
+        }
+    }
+}
